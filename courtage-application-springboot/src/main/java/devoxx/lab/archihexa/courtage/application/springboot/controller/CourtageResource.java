eb98878e13fa1abaa29320f941e19d48f9f65b97
jycr <jycr@users.noreply.github.com>
2022-03-11 10:25:18 +0100

    :bento: Ajout éléments de base pour l'étape 5

---
+++ courtage-application-springboot/src/main/java/devoxx/lab/archihexa/courtage/application/springboot/controller/CourtageResource.java
@@ -0,0 +1,18 @@
+package devoxx.lab.archihexa.courtage.application.springboot.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.Optional.ofNullable;
+
+@RestController
+@RequestMapping("/courtage")
+public class CourtageResource {
+	@GetMapping(path = "/version", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String version() {
+		return ofNullable(CourtageResource.class.getPackage().getImplementationVersion())
+			.orElse("DEV");
+	}
+}
