a521d61a4a4fcca13ed0fd2c72116e24a0eafab0
jycr <jycr@users.noreply.github.com>
2022-03-11 10:25:18 +0100

    :bento: Ajout éléments de base pour l'étape 5

--- courtage-application-springboot/src/test/java/devoxx/lab/archihexa/courtage/application/springboot/RunCucumberCourtageSpringbootApplicationTest.java
+++ courtage-application-springboot/src/test/java/devoxx/lab/archihexa/courtage/application/springboot/RunCucumberCourtageSpringbootApplicationTest.java
@@ -1,4 +1,4 @@
-package devoxx.lab.archihexa.courtage.domain;
+package devoxx.lab.archihexa.courtage.application.springboot;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -8,8 +8,9 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 
 @Suite
 @SelectClasspathResource("devoxx/lab/archihexa/courtage/domain")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "devoxx.lab.archihexa.courtage.domain")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "devoxx.lab.archihexa.courtage.application.springboot")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 @ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "false")
-public class RunCucumberCourtageDomainTest {
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "devoxx.lab.archihexa.courtage.application.springboot.CucumberLifecycleHandler")
+public class RunCucumberCourtageSpringbootApplicationTest {
 }
