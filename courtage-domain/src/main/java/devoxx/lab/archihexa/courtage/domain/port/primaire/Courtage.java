0006eea5b21e3d3a3ea9acefdc93713be6c685c5
jycr <jycr@users.noreply.github.com>
2022-04-06 18:08:36 +0200

    :bento: Ajout solution étape 2

--- courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/port/primaire/Courtage.java
+++ courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/port/primaire/Courtage.java
@@ -1,15 +1,39 @@
 package devoxx.lab.archihexa.courtage.domain.port.primaire;
 
+import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleDejaExistantException;
+import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleNonGereException;
 import devoxx.lab.archihexa.courtage.domain.model.Portefeuille;
+import devoxx.lab.archihexa.courtage.domain.port.secondaire.PortefeuilleRepository;
+
+import java.math.BigDecimal;
 
 public class Courtage implements ServiceCourtage {
+	private final PortefeuilleRepository portefeuilleRepository;
+
+	public Courtage(PortefeuilleRepository portefeuilleRepository) {
+		this.portefeuilleRepository = portefeuilleRepository;
+	}
+
 	@Override
-	public Portefeuille creerPortefeuille(String nomPortefeuille) {
-		return new Portefeuille(nomPortefeuille);
+	public Portefeuille creerPortefeuille(String nomPortefeuille) throws PortefeuilleDejaExistantException {
+		if (gere(nomPortefeuille)) {
+			throw new PortefeuilleDejaExistantException();
+		}
+		Portefeuille nouveauPortefeuille = new Portefeuille(nomPortefeuille);
+		this.portefeuilleRepository.sauvegarde(nouveauPortefeuille);
+		return nouveauPortefeuille;
 	}
 
 	@Override
 	public boolean gere(String nomPortefeuille) {
-		return true;
+		return portefeuilleRepository.existe(nomPortefeuille);
+	}
+
+	@Override
+	public BigDecimal calculerValeurPortefeuille(String nomPortefeuille) throws PortefeuilleNonGereException {
+		if (!gere(nomPortefeuille)) {
+			throw new PortefeuilleNonGereException();
+		}
+		return BigDecimal.ZERO;
 	}
 }
--- courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/port/primaire/ServiceCourtage.java
+++ courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/port/primaire/ServiceCourtage.java
@@ -1,9 +1,15 @@
 package devoxx.lab.archihexa.courtage.domain.port.primaire;
 
+import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleDejaExistantException;
+import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleNonGereException;
 import devoxx.lab.archihexa.courtage.domain.model.Portefeuille;
 
+import java.math.BigDecimal;
+
 public interface ServiceCourtage {
-	Portefeuille creerPortefeuille(String nomPortefeuille);
+	Portefeuille creerPortefeuille(String nomPortefeuille) throws PortefeuilleDejaExistantException;
+
+	BigDecimal calculerValeurPortefeuille(String nomPortefeuille) throws PortefeuilleNonGereException;
 
 	boolean gere(String nomPortefeuille);
 }
