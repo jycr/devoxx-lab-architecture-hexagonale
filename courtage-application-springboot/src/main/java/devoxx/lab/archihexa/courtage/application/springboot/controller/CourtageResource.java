e82f9b9a4964ff9494ed0ec241f1ac01e947fd24
jycr <jycr@users.noreply.github.com>
2022-04-06 18:17:49 +0200

    :bento: Ajout solution étape 5

--- courtage-application-springboot/src/main/java/devoxx/lab/archihexa/courtage/application/springboot/controller/CourtageResource.java
+++ courtage-application-springboot/src/main/java/devoxx/lab/archihexa/courtage/application/springboot/controller/CourtageResource.java
@@ -1,5 +1,8 @@
 package devoxx.lab.archihexa.courtage.application.springboot.controller;
 
+import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleDejaExistantException;
+import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleNonGereException;
+import devoxx.lab.archihexa.courtage.domain.port.primaire.ServiceCourtage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +18,28 @@ import static java.util.Optional.ofNullable;
 @RestController
 @RequestMapping("/courtage")
 public class CourtageResource {
+	private final ServiceCourtage serviceCourtage;
+
+	public CourtageResource(ServiceCourtage serviceCourtage) {
+		this.serviceCourtage = serviceCourtage;
+	}
+
+	@GetMapping("/portefeuilles/{nomPortefeuille}")
+	public ResponseEntity<Void> portefeuilleExiste(@PathVariable(value = "nomPortefeuille") String nomPortefeuille) throws PortefeuilleNonGereException {
+		if (!serviceCourtage.gere(nomPortefeuille)) {
+			throw new PortefeuilleNonGereException();
+		}
+		return ResponseEntity.ok().build();
+	}
+
 	@GetMapping(path = "/version", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String version() {
 		return ofNullable(CourtageResource.class.getPackage().getImplementationVersion()).orElse("DEV");
 	}
 
 	@PostMapping("/portefeuilles/{nomPortefeuille}")
-	public ResponseEntity<String> creationPortefeuille(@PathVariable(value = "nomPortefeuille") String nomPortefeuille) {
-		// TODO
-
-		// Redirection vers l'URI de la ressource créé
+	public ResponseEntity<String> creationPortefeuille(@PathVariable(value = "nomPortefeuille") String nomPortefeuille) throws PortefeuilleDejaExistantException {
+		serviceCourtage.creerPortefeuille(nomPortefeuille);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build(nomPortefeuille);
 		return ResponseEntity.created(uri).build();
 	}
@@ -37,5 +52,17 @@ public class CourtageResource {
 			.body(e.getFieldErrors().stream()
 				.map(fe -> "\t" + fe.getField() + " " + fe.getDefaultMessage())
 				.collect(Collectors.joining("\n", "Donnée(s) erronée(s): \\n\"", "")));
+	}
+
+	@ExceptionHandler(PortefeuilleDejaExistantException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	ResponseEntity<String> handlePortefeuilleDejaExistantException(PortefeuilleDejaExistantException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Portefeuille déjà géré");
+	}
+
+	@ExceptionHandler(PortefeuilleNonGereException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	ResponseEntity<String> handlePortefeuilleNonGereException(PortefeuilleNonGereException e) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Portefeuille non géré");
 	}
 }
