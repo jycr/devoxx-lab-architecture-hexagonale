0676ed25a0200397608cdfe125142d0958964f76
jycr <jycr@users.noreply.github.com>
2022-04-06 18:12:20 +0200

    :bento: Ajout solution étape 3

--- courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/model/Portefeuille.java
+++ courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/model/Portefeuille.java
@@ -1,7 +1,14 @@
 package devoxx.lab.archihexa.courtage.domain.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.unmodifiableMap;
+import static java.util.Optional.ofNullable;
+
 public class Portefeuille {
 	private final String nom;
+	private final Map<String, Integer> actions = new HashMap<>();
 
 	public Portefeuille(String nom) {
 		this.nom = nom;
@@ -9,5 +16,16 @@ public class Portefeuille {
 
 	public String getNom() {
 		return nom;
+	}
+
+	public void ajouterAction(Achat achat) {
+		actions.compute(
+			achat.getAction(),
+			(action, ancienNbActions) -> ofNullable(ancienNbActions).orElse(0) + achat.getNombre()
+		);
+	}
+
+	public Map<String, Integer> getActions() {
+		return unmodifiableMap(actions);
 	}
 }
--- courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/port/primaire/ServiceCourtage.java
+++ courtage-domain/src/main/java/devoxx/lab/archihexa/courtage/domain/port/primaire/ServiceCourtage.java
@@ -2,6 +2,7 @@ package devoxx.lab.archihexa.courtage.domain.port.primaire;
 
 import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleDejaExistantException;
 import devoxx.lab.archihexa.courtage.domain.exception.PortefeuilleNonGereException;
+import devoxx.lab.archihexa.courtage.domain.model.Achat;
 import devoxx.lab.archihexa.courtage.domain.model.Portefeuille;
 
 import java.math.BigDecimal;
@@ -12,4 +13,6 @@ public interface ServiceCourtage {
 	BigDecimal calculerValeurPortefeuille(String nomPortefeuille) throws PortefeuilleNonGereException;
 
 	boolean gere(String nomPortefeuille);
+
+	void ajouteAction(String nomPortefeuille, Achat achat) throws PortefeuilleNonGereException;
 }
